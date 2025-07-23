937ef8f7c333fecb0591de54aa69ae251ca01fba
Mark <mark.inabnit@gmail.com>
2025-07-23 12:41:30 -0500

    0.3.0 - Added Data Split Step

--- main.py
+++ main.py
@@ -88,6 +88,16 @@ def go(config: DictConfig):
         if "data_split" in active_steps:
             ##################
             # Implement here #
+            mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                }
+            )
             ##################
             pass
 
