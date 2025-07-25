c66cf0f236c79f8a1a4f2f926a81184d57336400
Mark <mark.inabnit@gmail.com>
2025-07-25 13:40:25 -0500

    1.0.1 - changed from OrdinalEncoder to OneHotEncoder to comply with project rubric.  Retrained and selected new prod model.  Retested regression model

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -166,7 +166,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
         SimpleImputer(strategy="most_frequent"),
-        OrdinalEncoder()
+        #OrdinalEncoder()
+        OneHotEncoder()
     )
     ######################################
 
